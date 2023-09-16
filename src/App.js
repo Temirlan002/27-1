6cde1a5dfdd10da533bc32173661444f7e15fed5
Tima19 <temirlan.baltabaev6@gmail.com>
2023-09-16 20:16:06 +0600

    lesson 3

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AboutPage from './pages/aboutPage/AboutPage';
+import MainPage from './pages/mainPage/MainPage';
+import UsersPage from './pages/usersPage/UsersPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+      <AboutPage />
+      <UsersPage />
+    </div>
+  );
+}
+
+export default App;
