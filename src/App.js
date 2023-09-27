4724dcd4c5ca9cf939faa1feb44c0e02f9a41c1e
Temirlan002 <86610538+Temirlan002@users.noreply.github.com>
2023-09-27 19:46:04 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-<<<<<<< Updated upstream
 import AboutPage from './pages/aboutPage/AboutPage';
 import MainPage from './pages/mainPage/MainPage';
 import UsersPage from './pages/usersPage/UsersPage';
 
-function App() {
-  return (
-    <div className="App">
-      <MainPage />
-      <AboutPage />
-      <UsersPage />
-=======
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PostPage from './pages/postPage/PostPage';
 import CreatePostsPage from './pages/createPostsPage/CreatePostsPage';
@@ -20,7 +13,6 @@ function App() {
     <div className='App' >
       <PostPage />
       {/* <CreatePostsPage /> */}
->>>>>>> Stashed changes
     </div>
   );
 }
